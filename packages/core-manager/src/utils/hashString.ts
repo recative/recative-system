7c96558cf386577f60ca109f1ccefd4a86dbdd53
LOSSES Don <1384036+Losses@users.noreply.github.com>
2022-08-04 13:44:36 +0800

    chore: Add a hash string method

---
+++ packages/core-manager/src/utils/hashString.ts
@@ -0,0 +1,13 @@
+export const hashString = (x: string) =>  {
+  let hash = 0, i, chr;
+
+  if (x.length === 0) return hash;
+
+  for (i = 0; i < x.length; i += 1) {
+    chr = x.charCodeAt(i);
+    hash = ((hash << 5) - hash) + chr;
+    hash |= 0;
+  }
+
+  return hash;
+};
