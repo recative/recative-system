a4362996b4476600f53306c4a76808493e258a6e
LOSSES Don <1384036+Losses@users.noreply.github.com>
2022-08-08 13:22:42 +0800

    feat: Supports USON data format

---
+++ packages/client-sdk/src/utils/fetchUson.ts
@@ -0,0 +1,14 @@
+import debug from 'debug';
+import { parse } from '@recative/ugly-json';
+
+const log = debug('client:fetch');
+
+export const fetchUson = async <T>(url: string) => {
+  const response = await fetch(url);
+  log(`Got response for file: ${url}`);
+
+  const raw = await response.text();
+  const data = parse(raw) as T;
+  log(`fetched ${url}, with data:`, data);
+  return data as T;
+};
