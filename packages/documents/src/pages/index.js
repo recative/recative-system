4591a383c56e2e7bdcadfa7a417a7c643a54e969
LOSSES Don <1384036+Losses@users.noreply.github.com>
2022-08-27 00:28:40 +0800

    chore(documents): Update page title

--- packages/documents/src/pages/index.js
+++ packages/documents/src/pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title}`}
+      title="Home"
       description="The homepage of Recative System">
       <HomepageHeader />
       <main>
