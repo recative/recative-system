175345ec4aef553fca206aed343c1a3cc5cb290a
LOSSES Don <1384036+Losses@users.noreply.github.com>
2022-09-05 22:24:19 +0800

    chore: Package maintainence

--- packages/web-shell/src/index.tsx
+++ packages/web-shell/src/index.tsx
@@ -19,17 +19,33 @@ const engine = new Styletron();
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
-root.render(
-  <HashRouter>
-    <PlayerSdkProvider
-      pathPattern={pathPattern}
-      dataType={dataType}
-    >
+
+fetch(`/bundle/data/constants.json`)
+  .then((response) => response.json())
+  .then((data) => {
+    Reflect.set(window, 'constant', data);
+
+    if (typeof data === 'object' && data !== null) {
+      if (typeof data.localStorage === 'object' && data.localStorage !== null) {
+        Object.keys(data.localStorage).forEach((key) => {
+          localStorage.setItem(key, data.localStorage[key]);
+        });
+      }
+    }
+  })
+  .finally(() => {
+    root.render(
       <StyletronProvider value={engine}>
         <BaseProvider theme={DarkTheme}>
-          <App />
+          <HashRouter>
+            <PlayerSdkProvider
+              pathPattern={pathPattern}
+              dataType={dataType}
+            >
+              <App />
+            </PlayerSdkProvider>
+          </HashRouter>,
         </BaseProvider>
       </StyletronProvider>
-    </PlayerSdkProvider>
-  </HashRouter>,
-);
+    );
+  });
