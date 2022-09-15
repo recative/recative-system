c98d4cb91eae486be3fcfd32101911f76392809d
LOSSES Don <1384036+Losses@users.noreply.github.com>
2022-09-15 20:13:24 +0800

    chore(client-sdk): Better episodeID detection method

---
+++ .changeset/flat-comics-cry.md
@@ -0,0 +1,5 @@
+---
+'@recative/client-sdk': patch
+---
+
+chore: Better episodeID detection method
--- packages/client-sdk/src/components/hooks/useEpisodeIdNormalizer.ts
+++ packages/client-sdk/src/components/hooks/useEpisodeIdNormalizer.ts
@@ -2,18 +2,14 @@ import * as React from 'react';
 
 import { useSdkConfig } from '../../hooks/useSdkConfig';
 
-const isNumeric = (value: string) => {
-  return /^-?\d+$/.test(value);
-};
-
 export const useEpisodeIdNormalizer = () => {
   const config = useSdkConfig();
 
   return React.useCallback((episodeId: string | undefined) => {
     if (!episodeId) return episodeId;
 
-    const nEpisodeId = Number.parseInt(episodeId, 10);
-    const parsedEpisodeId = isNumeric(episodeId) && Number.isNaN(nEpisodeId)
+    const nEpisodeId = Number(episodeId);
+    const parsedEpisodeId = Number.isNaN(nEpisodeId)
       ? episodeId
       : config.episodeOrderToEpisodeIdMap.get(nEpisodeId);
 
