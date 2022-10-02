bda7d94d304134b070bbc3a74f57a99cba71c9f7
LOSSES Don <1384036+Losses@users.noreply.github.com>
2022-10-02 10:36:18 +0800

    chore(tsdb): Initialize the project

---
+++ packages/tsdb/README.md
@@ -0,0 +1,12 @@
+# tsdb
+
+A refactored version of Loki.js with modern API, build-in TypeScript support,
+more unit tests and bug fixes.
+
+## Building
+
+Run `nx build tsdb` to build the library.
+
+## Running unit tests
+
+Run `nx test tsdb` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/tsdb/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'tsdb',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/tsdb',
+};
