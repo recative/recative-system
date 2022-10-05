323722afbf59f28a4d448ee3fc7d8ba7bed0ccdb
LOSSES Don <1384036+Losses@users.noreply.github.com>
2022-10-05 13:05:05 +0800

    feat(database): Refactor the DynamicView class

--- packages/database/src/utils/delay.ts
+++ packages/database/src/utils/delay.ts
@@ -1,13 +1,13 @@
-export const delay = <P>(x: (() => P)): Promise<P> => {
+export const delay = <P>(x: () => P, time = 1): Promise<P> => {
   return new Promise<P>((resolve, reject) => {
     globalThis.setTimeout(() => {
       if (typeof x === 'function') {
         resolve(x());
       } else {
         reject(
-          new TypeError('Argument passed for async execution is not a function'),
+          new TypeError('Argument passed for async execution is not a function')
         );
       }
-    }, 1);
+    }, time);
   });
 };
