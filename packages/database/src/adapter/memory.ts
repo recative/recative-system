7d13f778ed1b098d96d96b87f63854df718d8f9a
LOSSES Don <1384036+Losses@users.noreply.github.com>
2022-10-07 14:18:24 +0800

    feat(database): Refactor the memory adapter

---
+++ packages/database/src/adapter/memory.ts
@@ -0,0 +1,58 @@
+export interface IHashStore {
+  saveCount: number;
+  lastSave: Date;
+  value: string;
+}
+
+/**
+ * In in-memory persistence adapter for an in-memory database.
+ * This simple 'key/value' adapter is intended for unit testing and diagnostics.
+ */
+export class LokiMemoryAdapter {
+  hashStore = new Map<string, IHashStore>();
+
+  /**
+   * Loads a serialized database from its in-memory store.
+   *
+   * @param databaseName - name of the database (fileName/keyName)
+   */
+  loadDatabase = (databaseName: string) => {
+    return new Promise((resolve) => {
+      const hash = this.hashStore.get(databaseName);
+      if (hash) {
+        resolve(hash.value);
+      } else {
+        // database doesn't exist, return falsy
+        resolve(null);
+      }
+    });
+  };
+
+  /**
+   * Saves a serialized database to its in-memory store.
+   *
+   * @param databaseName - name of the database (fileName/keyName)
+   */
+  saveDatabase = (databaseName: string, databaseString: string) => {
+    return new Promise<void>((resolve) => {
+      const hash = this.hashStore.get(databaseName);
+
+      this.hashStore.set(databaseName, {
+        saveCount: (hash?.saveCount ?? 0) + 1,
+        lastSave: new Date(),
+        value: databaseString
+      });
+
+      resolve();
+    });
+  };
+
+  /**
+   * Deletes a database from its in-memory store.
+   *
+   * @param {string} databaseName - name of the database (fileName/keyName)
+   */
+  deleteDatabase = (databaseName: string) => {
+    return Promise.resolve(this.hashStore.delete(databaseName));
+  };
+}
