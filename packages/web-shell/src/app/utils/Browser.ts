9beba1d4c823609f48e3d1f217ecd91ed2f4ffa2
Ben Pig Chu <benpichu@gmail.com>
2022-11-02 16:44:41 +0800

    feat(web-shell): Allow secureBrowser in dependencies to detect window close

--- packages/web-shell/src/app/utils/Browser.ts
+++ packages/web-shell/src/app/utils/Browser.ts
@@ -15,29 +15,59 @@ export const InAppBrowser = {
   }
 };
 
+
+let lastWindow: WindowProxy | null = null
+
+let browserFinishedListeners: (() => void)[] = []
+
+let checkWindowClosedInterval: ReturnType<typeof setInterval> | null = null
+
+// Its impossible to add event listener to a cross domain window, so query it every second
+const checkWindowClosed = () => {
+  if (lastWindow?.closed) {
+    lastWindow = null;
+    browserFinishedListeners.forEach((listener) => {
+      listener()
+    })
+  }
+}
+
+const closeWindow = () => {
+  if (lastWindow !== null) {
+    lastWindow.close();
+    lastWindow = null;
+    browserFinishedListeners.forEach((listener) => {
+      listener()
+    })
+  }
+  clearInterval(checkWindowClosedInterval ?? undefined)
+}
+
 /**
  * Another minimal utility used to open a new tab
  * compatible with implementation in @capacitor/browser.
  */
 export const Browser = {
-  _lastWindow: null as WindowProxy | null,
   open: async (option: {
     url: string,
     windowName?: string,
   }) => {
-    Browser._lastWindow = window.open(option.url, option.windowName ?? '_blank');
+    closeWindow()
+    lastWindow = window.open(option.url, option.windowName ?? '_blank');
+    checkWindowClosedInterval = setInterval(checkWindowClosed, 1000)
   },
   close: async () => {
-    if (Browser._lastWindow === null) {
+    if (lastWindow === null) {
       throw new Error('No active window to close!');
     }
-    Browser._lastWindow.close();
-    Browser._lastWindow = null;
+    closeWindow()
   },
-  addListener: () => {
-    // do nothing
+  addListener: (type: string, listener: () => void) => {
+    if (type === "browserFinished") {
+      browserFinishedListeners.push(listener)
+    }
   },
   removeAllListeners: () => {
-    // do nothing
+    browserFinishedListeners = []
   },
 }
