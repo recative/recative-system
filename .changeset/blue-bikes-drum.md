604006604c26eb6fd6ba2d3e7d3f220f0f2ac453
LOSSES Don <1384036+Losses@users.noreply.github.com>
2022-12-16 14:58:08 +0800

    feat: Add deprecated flag for releases

---
+++ .changeset/blue-bikes-drum.md
@@ -0,0 +1,5 @@
+---
+'@recative/definitions': patch
+---
+
+feat: Add deprecated flag for releases
--- packages/definitions/src/typings/meta/release.ts
+++ packages/definitions/src/typings/meta/release.ts
@@ -1,16 +1,70 @@
+/**
+ * Represents a media or code release release.
+ */
 export interface ISimpleRelease {
+  /**
+   * The unique identifier for the release.
+   */
   readonly id: number;
+
+  /**
+   * The name of the committer who created the release.
+   */
   committer: string;
+
+  /**
+   * The timestamp for when the release was created.
+   */
   commitTime: number;
+
+  /**
+   * Whether the release has been deprecated.
+   */
+  deprecated: boolean;
+
+  /**
+   * Notes associated with the release.
+   */
   notes: string;
 }
 
+/**
+ * Represents a release that includes both code and media.
+ */
 export interface IBundleRelease {
+  /**
+   * The unique identifier for the release.
+   */
   readonly id: number;
+
+  /**
+   * The unique identifier for the code build associated with the release.
+   */
   readonly codeBuildId: number;
+
+  /**
+   * The unique identifier for the media build associated with the release.
+   */
   readonly mediaBuildId: number;
+
+  /**
+   * The name of the committer who created the release.
+   */
   committer: string;
+
+  /**
+   * The timestamp for when the release was created.
+   */
   commitTime: number;
+
+  /**
+   * Whether the release has been deprecated.
+   */
+  deprecated: boolean;
+
+  /**
+   * Notes associated with the release.
+   */
   notes: string;
 }
 
@@ -18,6 +72,7 @@ export interface ISeriesMetadata {
   readonly id: string;
   title: Record<string, string>;
   description: Record<string, string>;
+  deprecated: boolean;
   loadingCoverForCatalogPageResourceId: string;
   loadingCoverForMainContentsResourceId: string;
 }
