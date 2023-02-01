beb2b1bf3d7a7057ca8496dc9deab1a655152afe
Ben Pig Chu <benpichu@gmail.com>
2023-02-01 16:15:54 +0800

    fix(desktop-shell,web-shell): Initial episode id not working

---
+++ .changeset/clean-llamas-pay.md
@@ -0,0 +1,6 @@
+---
+'@recative/desktop-shell': patch
+'@recative/web-shell': patch
+---
+
+fix: Initial episode id not working
--- packages/web-shell/src/app/constants/storageKeys.ts
+++ packages/web-shell/src/app/constants/storageKeys.ts
@@ -1,7 +1,8 @@
 export const PATH = '@recative/shell/path';
 export const DATA_TYPE = '@recative/shell/data-type';
-export const INDEX_ORDER = '@recative/shell/index-order';
+export const INDEX_ORDER = '@recative/mobile-shell/index-order';
 export const PREFERRED_UPLOADERS = '@recative/shell/preferred-uploaders';
 export const CONTAINER_COMPONENT = '@recative/shell/container-component';
-export const REPORT_RESOURCE_VALIDATION = '@recative/shell/report-resource-validation';
+export const REPORT_RESOURCE_VALIDATION =
+  '@recative/shell/report-resource-validation';
 export const ADDITIONAL_CONFIG = '@recative/shell/additional-config';
